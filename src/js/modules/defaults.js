2fda97bfd0ecf0f7805e62362874392577a27225
Ryan Van Ess <fallen.rve@gmail.com>
2017-02-05 19:28:40 -0600

    adds default html

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -1,3 +1,4 @@
+import {podalClasses} from './classes';
 let defaults = {
     speed: 100, // in ms
     delay: 100, // in ms
@@ -10,5 +11,21 @@ let defaults = {
     preProcess:  () => { },
     postProcess: () => { },
 };
+
+const podalHTML = `
+<div class='${podalClasses.wrapper}'>
+    <div class='${podalClasses.box}'>
+        <div class='${podalClasses.loading} ${podalClasses.alert}'>
+            <div class='${podalClasses.loader}'></div>
+            <i class='fa fa-processing'></i>
+            <div class='button-wrapper'>
+                <button class"${podalClasses.cancel}"=>Cancel</button>
+                <button class"${podalClasses.confirm}"=>OK</button>
+                <button class"${podalClasses.delete}"=>Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+`.replace(/(^|\n)\s*/g, '');
 
 export default defaults;
