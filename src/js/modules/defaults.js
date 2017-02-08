faded68e36a508966e4611cd9471ee1cb2acf2c7
Ryan Van Ess <fallen.rve@gmail.com>
2017-02-07 21:31:42 -0600

    updates default html

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -13,16 +13,14 @@ let defaults = {
 };
 
 const podalHTML = `
-<div class='${podalClasses.wrapper}'>
-    <div class='${podalClasses.box}'>
-        <div class='${podalClasses.loading} ${podalClasses.alert}'>
-            <div class='${podalClasses.loader}'></div>
-            <i class='fa fa-processing'></i>
-            <div class='button-wrapper'>
-                <button class"${podalClasses.cancel}"=>Cancel</button>
-                <button class"${podalClasses.confirm}"=>OK</button>
-                <button class"${podalClasses.delete}"=>Delete</button>
-            </div>
+<div class='${podalClasses.box}'>
+    <div class='${podalClasses.loading} ${podalClasses.alert}'>
+        <div class='${podalClasses.loader}'></div>
+        <i class='fa fa-processing'></i>
+        <div class='button-wrapper'>
+            <button class"${podalClasses.cancel}"=>Cancel</button>
+            <button class"${podalClasses.confirm}"=>OK</button>
+            <button class"${podalClasses.delete}"=>Delete</button>
         </div>
     </div>
 </div>
